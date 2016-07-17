cae1dc0d0f151cf534407aadddc8d9a22823e7c7
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-07-17 01:25:07 -0300

    Stock Symbol not found.

--- app/src/main/java/com/sam_chordas/android/stockhawk/rest/Utils.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/rest/Utils.java
@@ -1,7 +1,13 @@
 package com.sam_chordas.android.stockhawk.rest;
 
 import android.content.ContentProviderOperation;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.data.QuoteColumns;
 import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 import java.util.ArrayList;
@@ -14,82 +20,108 @@ import org.json.JSONObject;
  */
 public class Utils {
 
-  private static String LOG_TAG = Utils.class.getSimpleName();
+	private static String LOG_TAG = Utils.class.getSimpleName();
 
-  public static boolean showPercent = true;
+	public static boolean showPercent = true;
 
-  public static ArrayList quoteJsonToContentVals(String JSON){
-    ArrayList<ContentProviderOperation> batchOperations = new ArrayList<>();
-    JSONObject jsonObject = null;
-    JSONArray resultsArray = null;
-    try{
-      jsonObject = new JSONObject(JSON);
-      if (jsonObject != null && jsonObject.length() != 0){
-        jsonObject = jsonObject.getJSONObject("query");
-        int count = Integer.parseInt(jsonObject.getString("count"));
-        if (count == 1){
-          jsonObject = jsonObject.getJSONObject("results")
-              .getJSONObject("quote");
-          batchOperations.add(buildBatchOperation(jsonObject));
-        } else{
-          resultsArray = jsonObject.getJSONObject("results").getJSONArray("quote");
+	public static ArrayList quoteJsonToContentVals(Context context, String JSON){
+		ArrayList<ContentProviderOperation> batchOperations = new ArrayList<>();
+		JSONObject jsonObject = null;
+		JSONArray resultsArray = null;
+		try{
+			jsonObject = new JSONObject(JSON);
+			if (jsonObject != null && jsonObject.length() != 0){
+				jsonObject = jsonObject.getJSONObject("query");
+				int count = Integer.parseInt(jsonObject.getString("count"));
+				if (count == 1){
+					jsonObject = jsonObject.getJSONObject("results")
+							.getJSONObject("quote");
+					addToList(context, jsonObject, batchOperations);
+				} else{
+					resultsArray = jsonObject.getJSONObject("results").getJSONArray("quote");
 
-          if (resultsArray != null && resultsArray.length() != 0){
-            for (int i = 0; i < resultsArray.length(); i++){
-              jsonObject = resultsArray.getJSONObject(i);
-              batchOperations.add(buildBatchOperation(jsonObject));
-            }
-          }
-        }
-      }
-    } catch (JSONException e){
-      Log.e(LOG_TAG, "String to JSON failed: " + e);
-    }
-    return batchOperations;
-  }
+					if (resultsArray != null && resultsArray.length() != 0){
+						for (int i = 0; i < resultsArray.length(); i++){
+							jsonObject = resultsArray.getJSONObject(i);
+							addToList(context, jsonObject, batchOperations);
+						}
+					}
+				}
+			}
+		} catch (JSONException e){
+			Log.e(LOG_TAG, "String to JSON failed: " + e);
+		}
+		return batchOperations;
+	}
 
-  public static String truncateBidPrice(String bidPrice){
-    bidPrice = String.format("%.2f", Float.parseFloat(bidPrice));
-    return bidPrice;
-  }
+	public static String truncateBidPrice(String bidPrice){
+		/*Todo - DEBUG - java.lang.NumberFormatException: Invalid float: "null"*/
+		bidPrice = String.format("%.2f", Float.parseFloat(bidPrice));
+		return bidPrice;
+	}
 
-  public static String truncateChange(String change, boolean isPercentChange){
-    String weight = change.substring(0,1);
-    String ampersand = "";
-    if (isPercentChange){
-      ampersand = change.substring(change.length() - 1, change.length());
-      change = change.substring(0, change.length() - 1);
-    }
-    change = change.substring(1, change.length());
-    double round = (double) Math.round(Double.parseDouble(change) * 100) / 100;
-    change = String.format("%.2f", round);
-    StringBuffer changeBuffer = new StringBuffer(change);
-    changeBuffer.insert(0, weight);
-    changeBuffer.append(ampersand);
-    change = changeBuffer.toString();
-    return change;
-  }
+	public static String truncateChange(String change, boolean isPercentChange){
+		String weight = change.substring(0,1);
+		String ampersand = "";
+		if (isPercentChange){
+			ampersand = change.substring(change.length() - 1, change.length());
+			change = change.substring(0, change.length() - 1);
+		}
+		change = change.substring(1, change.length());
+		double round = (double) Math.round(Double.parseDouble(change) * 100) / 100;
+		change = String.format("%.2f", round);
+		StringBuffer changeBuffer = new StringBuffer(change);
+		changeBuffer.insert(0, weight);
+		changeBuffer.append(ampersand);
+		change = changeBuffer.toString();
+		return change;
+	}
 
-  public static ContentProviderOperation buildBatchOperation(JSONObject jsonObject){
-    ContentProviderOperation.Builder builder = ContentProviderOperation.newInsert(
-        QuoteProvider.Quotes.CONTENT_URI);
-    try {
-      String change = jsonObject.getString("Change");
-      builder.withValue(QuoteColumns.SYMBOL, jsonObject.getString("symbol"));
-      builder.withValue(QuoteColumns.BIDPRICE, truncateBidPrice(jsonObject.getString("Bid")));
-      builder.withValue(QuoteColumns.PERCENT_CHANGE, truncateChange(
-          jsonObject.getString("ChangeinPercent"), true));
-      builder.withValue(QuoteColumns.CHANGE, truncateChange(change, false));
-      builder.withValue(QuoteColumns.ISCURRENT, 1);
-      if (change.charAt(0) == '-'){
-        builder.withValue(QuoteColumns.ISUP, 0);
-      }else{
-        builder.withValue(QuoteColumns.ISUP, 1);
-      }
+	public static ContentProviderOperation buildBatchOperation(JSONObject jsonObject) {
+		ContentProviderOperation.Builder builder = ContentProviderOperation.newInsert(
+				QuoteProvider.Quotes.CONTENT_URI);
+		Log.d("json", jsonObject.toString());
+		try {
+			String change = jsonObject.getString("Change");
 
-    } catch (JSONException e){
-      e.printStackTrace();
-    }
-    return builder.build();
-  }
+			if (change.equals("null")) {
+				return null;
+			}
+
+			builder.withValue(QuoteColumns.SYMBOL, jsonObject.getString("symbol"));
+			builder.withValue(QuoteColumns.BIDPRICE, truncateBidPrice(jsonObject.getString("Bid")));
+			builder.withValue(QuoteColumns.PERCENT_CHANGE, truncateChange(
+					jsonObject.getString("ChangeinPercent"), true));
+			builder.withValue(QuoteColumns.CHANGE, truncateChange(change, false));
+			builder.withValue(QuoteColumns.ISCURRENT, 1);
+			if (change.charAt(0) == '-'){
+				builder.withValue(QuoteColumns.ISUP, 0);
+			}else{
+				builder.withValue(QuoteColumns.ISUP, 1);
+			}
+
+		} catch (JSONException e){
+			e.printStackTrace();
+		}
+		return builder.build();
+	}
+
+	public static void addToList(final Context context, JSONObject jsonObject, ArrayList<ContentProviderOperation> batchOperations) {
+		ContentProviderOperation operation = buildBatchOperation(jsonObject);
+		if (operation != null) {
+			batchOperations.add(operation);
+		} else {
+			Handler handler = new Handler(Looper.getMainLooper());
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+					Toast.makeText(context.getApplicationContext(),
+							R.string.toast_symbol_not_found,
+							Toast.LENGTH_LONG)
+							.show();
+				}
+			});
+			Log.e(LOG_TAG, "Cannot find new symbol");
+		}
+	}
 }
