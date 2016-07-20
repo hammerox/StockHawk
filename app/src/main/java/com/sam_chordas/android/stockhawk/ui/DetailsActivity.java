34ff187c4401317cdede9f045621206752bdb2ad
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-07-20 01:31:54 -0300

    Random data generator

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsActivity.java
@@ -1,13 +1,25 @@
 package com.sam_chordas.android.stockhawk.ui;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.CandleStickChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.CandleData;
+import com.github.mikephil.charting.data.CandleDataSet;
+import com.github.mikephil.charting.data.CandleEntry;
 import com.sam_chordas.android.stockhawk.R;
 
+import java.util.ArrayList;
+
 public class DetailsActivity extends Activity {
+
+    private CandleStickChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,73 @@ public class DetailsActivity extends Activity {
         titleView.setText(symbol);
 
         // Graph
-        CandleStickChart chart = (CandleStickChart) findViewById(R.id.chart);
+        mChart = (CandleStickChart) findViewById(R.id.chart);
+        mChart.setMaxVisibleValueCount(20);
+        mChart.setDrawGridBackground(false);
+        mChart.getLegend().setEnabled(false);
+        mChart.setDescription("USD");
+        mChart.setDescriptionColor(Color.WHITE);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+        xAxis.setDrawGridLines(false);
+        xAxis.setTextColor(Color.WHITE);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setEnabled(true);
+        leftAxis.setLabelCount(7, false);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setDrawAxisLine(true);
+        leftAxis.setTextColor(Color.WHITE);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(true);
+        rightAxis.setLabelCount(7, false);
+        rightAxis.setDrawAxisLine(true);
+        rightAxis.setTextColor(Color.WHITE);
+
+        setData();
     }
+
+
+    public void setData() {
+        mChart.resetTracking();
+
+        ArrayList<CandleEntry> yVals1 = new ArrayList<>();
+
+        int xProg = 20;
+        int yProg = 100;
+
+        for (int i = 0; i < xProg; i++) {
+            float mult = (yProg);
+            float val = (float) (Math.random() * 40) + mult;
+
+            float high = (float) (Math.random() * 9) + 8f;
+            float low = (float) (Math.random() * 9) + 8f;
+
+            float open = (float) (Math.random() * 6) + 1f;
+            float close = (float) (Math.random() * 6) + 1f;
+
+            boolean even = i % 2 == 0;
+
+            yVals1.add(new CandleEntry(i, val + high, val - low, even ? val + open : val - open,
+                    even ? val - close : val + close));
+        }
+
+        CandleDataSet set1 = new CandleDataSet(yVals1, "Data Set");
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setShadowColor(Color.WHITE);
+        set1.setShadowWidth(2f);
+        set1.setDecreasingColor(Color.RED);
+        set1.setDecreasingPaintStyle(Paint.Style.FILL);
+        set1.setIncreasingColor(Color.rgb(122, 242, 84));
+        set1.setIncreasingPaintStyle(Paint.Style.FILL);
+        set1.setNeutralColor(Color.BLUE);
+
+        CandleData data = new CandleData(set1);
+
+        mChart.setData(data);
+        mChart.invalidate();
+    }
+
 }
