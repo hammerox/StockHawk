cb8df31a77b74d1ba73f88040b5ecef73b53b55e
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-07-20 00:22:29 -0300

    MPAndroidChart library

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.CandleStickChart;
 import com.sam_chordas.android.stockhawk.R;
 
 public class DetailsActivity extends Activity {
@@ -13,8 +14,12 @@ public class DetailsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        // Title
         String symbol = getIntent().getStringExtra(MyStocksActivity.KEY_STOCK_SYMBOL);
         TextView titleView = (TextView) findViewById(R.id.details_title);
         titleView.setText(symbol);
+
+        // Graph
+        CandleStickChart chart = (CandleStickChart) findViewById(R.id.chart);
     }
 }
